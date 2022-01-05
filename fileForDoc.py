56caa85276fe32cd328375901f4670bf44a0a3a9
pavankumar kosaraju <pkosaraj@cmhc-schl.gc.ca>
2022-01-05 07:56:29 -0500

    errors rectified

--- fileForDoc.py
+++ fileForDoc.py
@@ -36,8 +36,8 @@ from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33, random_state=100)
 
 # fitting the data to the Linear regression model
-from sklearn.linear_model import LinearRegression
-reg= LinearRegression()
+from sklearn import linear_model
+reg= linear_model.LinearRegression()
 reg.fit(x_train,y_train)
 
 #calucltaing the accuracy of the model
